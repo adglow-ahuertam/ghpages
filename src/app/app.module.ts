8576dd3220bf8cd2d45059fe27a69271bd6e7fb5
ahuerta <ahuerta@adglow.com>
2018-01-11 14:52:46 +0100

    basic components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeadComponent } from './head/head.component';
+import { BodyComponent } from './body/body.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
